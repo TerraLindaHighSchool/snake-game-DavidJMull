9215e17dc89cde3b9158fc5bc4526194275eba97
dm00967 <djm94903@gmail.com>
2019-01-14 14:05:28 -0800

    LAB 1

--- app/src/main/java/com/example/bruce/snake_startercode/SnakeSegment.java
+++ app/src/main/java/com/example/bruce/snake_startercode/SnakeSegment.java
@@ -1,4 +1,43 @@
 package com.example.bruce.snake_startercode;
 
 public class SnakeSegment {
+    public enum BodyParts{HEAD, BODY, TAIL};
+    BodyParts mbodyParts;
+    int mDegrees, mXLoc, mYLoc;
+
+    public SnakeSegment( BodyParts bodyParts , int Degrees, int XLoc, int YLoc){
+        mbodyParts = bodyParts;
+        mDegrees = Degrees;
+        mXLoc = XLoc;
+        mYLoc = YLoc;
+    };
+
+    public int getDegrees() {
+        return mDegrees;
+    }
+
+    public void setDegrees(int degrees) {
+        mDegrees = degrees;
+    }
+
+    public int getXLoc() {
+        return mXLoc;
+    }
+
+    public void setXLoc(int XLoc) {
+        mXLoc = XLoc;
+    }
+
+    public int getYLoc() {
+        return mYLoc;
+    }
+
+    public void setYLoc(int YLoc) {
+        mYLoc = YLoc;
+    }
+
+    public BodyParts getMbodyParts() {
+        return mbodyParts;
+    }
+
 }
